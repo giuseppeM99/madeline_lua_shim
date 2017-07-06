f2a1b6b9c661ea2d98dd8e96bce050723aec7e47
giuseppeM99 <giuseppe.marino1999@gmail.com>
2017-07-06 23:57:35 +0200

    Fixes to mediainfo

--- mediainfo.lua
+++ mediainfo.lua
@@ -6,13 +6,15 @@ local mediainfo = function(file)
   if not file then
     return false
   end
-  local han = io.popen("mediainfo -f " .. file)
+  local han = io.popen("mediainfo -f '" .. file .. "'")
   local info = han:read("*all")
   han:close()
   return {
     info = info,
     getDuration = function(self)
-      return math.ceil(self.info:match("Duration %s+: (%d+)")/1000)
+      local Duration = self.info:match("Duration %s+: (%d+)")
+      if not Duration then return nil end
+      return math.ceil(Duration/1000)
     end,
     getWidth = function(self)
       return self.info:match("Width %s+: (%d+)")
@@ -24,10 +26,14 @@ local mediainfo = function(file)
       return self.info:match("Internet media type %s+ : ([%w/]+)")
     end,
     getTitle = function(self)
-      return self.info:match("Title %s+ : ([%w%s&%.]+%c)"):sub(1, -2)
+      local Title = self.info:match("Title %s+ : ([%w%s&%.]+%c)")
+      if not Title then return nil end
+      return Title:sub(1, -2)
     end,
     getPerformer = function(self)
-      return self.info:match("Performer %s+ : ([%w%s&%.]+%c)"):sub(1, -2)
+      local Performer = self.info:match("Performer %s+ : ([%w%s&%.]+%c)")
+      if not Performer then return nil end
+      return Performer:sub(1, -2)
     end,
   }
 end
