0dd2ecd10855f118499e01a93a80b56f4ca4f68e
giuseppeM99 <giuseppe.marino1999@gmail.com>
2017-07-08 16:53:09 +0200

    Avoiding fatal errors

--- userlogin.php
+++ userlogin.php
@@ -2,19 +2,34 @@
 require "vendor/autoload.php";
 $settings = ['app_info' => ['api_id' => 6, 'api_hash' => 'eb06d4abfb49dc3eeb1aeb98ae0f581e'], 'logger' => ['loglevel' => \danog\MadelineProto\Logger::ERROR]];
 $madeline = new \danog\MadelineProto\API($settings);
-$madeline->phone_login(readline('Enter your phone number: '));
-$authorization = $madeline->complete_phone_login(readline('Enter the code you received: '));
+try {
+    $madeline->phone_login(readline('Enter your phone number: '));
+} catch (danog\MadelineProto\RPCErrorException $e) {
+    die ($e->getMessage().PHP_EOL);
+}
+try {
+    $authorization = $madeline->complete_phone_login(readline('Enter the code you received: '));
+} catch (danog\MadelineProto\RPCErrorException $e) {
+    die ($e->getMessage().PHP_EOL);
+}
 if ($authorization['_'] === 'account.noPassword') {
     throw new \danog\MadelineProto\Exception('2FA is enabled but no password is set!');
 }
 if ($authorization['_'] === 'account.password') {
-    $authorization = $madeline->complete_2fa_login(readline('Please enter your password (hint '.$authorization['hint'].'): '));
+    try {
+        $authorization = $madeline->complete_2fa_login(readline('Please enter your password (hint '.$authorization['hint'].'): '));
+    } catch (danog\MadelineProto\RPCErrorException $e) {
+        die ($e->getMessage().PHP_EOL);
+    }
 }
 if ($authorization['_'] === 'account.needSignup') {
     $authorization = $madeline->complete_signup(readline('Please enter your first name: '), readline('Please enter your last name (can be empty): '));
 }
 
-echo var_dump($authorization).PHP_EOL;
+echo $authorization['_'] == "auth.authorization"? "LOGGED AS ".$authorization['user']['first_name']
+.(isset($authorization['user']['last_name']) ? " ".$authorization['user']['last_name'] : "")
+.(isset($authorization['user']['username']) ? " (".$authorization['user']['username'].")" : "")
+." [".$authorization['user']['id']."]\n" : "LOGIN ERROR, RETRY LATER\n";
 
 \danog\MadelineProto\Serialization::serialize('bot.madeline', $madeline);
 
